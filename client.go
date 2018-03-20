9aa97639a529393ee94255f5af3314142ba77ad1
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-03-20 12:26:19 +0300

    client: add accounts fetching

--- client.go
+++ client.go
@@ -809,3 +809,74 @@ func (c *Client) LightningWithdraw(asset string,
 
 	return resp.Data.Withdrawal, nil
 }
+
+// AccountsRequest is a query variables used in request user's account
+// balances
+type AccountsRequest struct {
+	Assets []string `json:"assets"`
+}
+
+// TODO(sergey.d) Add comment
+type Account struct {
+	// TODO(sergey.d) Add comment
+	Asset string
+
+	// TODO(sergey.d) Add comment
+	Address string
+
+	// TODO(sergey.d) Add comment
+	Available decimal.Decimal
+
+	// TODO(sergey.d) Add comment
+	Estimation decimal.Decimal
+
+	// TODO(sergey.d) Add comment
+	Freezed decimal.Decimal
+
+	// TODO(sergey.d) Add comment
+	Pending decimal.Decimal
+}
+
+// Accounts shows balances for the assets owned by loggedin user
+// using specified invoice.
+func (c *Client) Accounts(assets []string) ([]Account, error) {
+
+	var req request
+
+	req.Query = `
+		query Accounts($assets: [Asset!]!) {
+  			accounts( assets: $assets) {
+				asset, address, available, estimation, freezed
+  			}
+		}
+	`
+
+	req.Variables = AccountsRequest{
+		Assets: assets,
+	}
+
+	resp := struct {
+		responseBase
+		Data struct {
+			Accounts []Account `json:"accounts"`
+		}
+	}{}
+
+	respJSON, err := c.do(req)
+	if err != nil {
+		return []Account{},
+			errors.New("failed to do request: " + err.Error())
+	}
+
+	if err := json.Unmarshal(respJSON, &resp); err != nil {
+		return []Account{},
+			errors.New("failed to json.Unmarshal resp: " + err.Error())
+	}
+
+	if err := resp.Error(); err != nil {
+		return resp.Data.Accounts,
+			errors.New("exchange error: " + err.Error())
+	}
+
+	return resp.Data.Accounts, nil
+}
